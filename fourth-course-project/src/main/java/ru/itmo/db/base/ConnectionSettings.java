8c0cf5b9e7de5ce1d0af0ad093e71d67a3c70605
EnikeevAI <alexander.enikeev@gmail.com>
2022-12-28 13:33:21 +0300

    Добавление класса с конфигурацией подключения к БД

---
+++ fourth-course-project/src/main/java/ru/itmo/db/base/ConnectionSettings.java
@@ -0,0 +1,7 @@
+package ru.itmo.db.base;
+
+public class ConnectionSettings {
+    public static final String CONNECTION_STR = "jdbc:postgresql://localhost:5432/db_lessons";
+    public static final String LOGIN ="ifmo";
+    public static final String PASSWORD = "qwer";
+}
